7b1a0fc19647c2c325e24c93c1440f06c2ede708
Germansdev <germanschw@yahoo.com>
2023-06-14 03:11:49 -0300

    resolved navigation bottomBar

--- app/src/main/java/com/example/games/ui/GameApp.kt
+++ app/src/main/java/com/example/games/ui/GameApp.kt
@@ -6,39 +6,36 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.material.SnackbarDefaults.backgroundColor
-import androidx.compose.material3.ListItemDefaults.contentColor
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.games.GameNavHost
+import com.example.games.R
 import com.example.games.appDestinations.BottomBarScreen
 import com.example.games.appDestinations.BottomBarScreen.*
-import com.example.games.ui.screens.HomeScreen
-import com.example.games.ui.theme.md_theme_light_surfaceTint
 
 //import com.example.games.GameNavHost
 
 //TOP BAR COMPOSABLE TO NAVIGATE CUSTOM:
 /**
 enum class NavigationScreens(@StringRes val tittle: Int) {
-    HomeScreen(tittle = R.string.homescreen),
-    ListScreen(tittle = R.string.app_name),
-    FavoriteScreen(tittle = R.string.favorites),
-    PlayedScreen(tittle = R.string.played),
-    SharedScreen(tittle = R.string.shared),
-    RatedScreen(tittle = R.string.rated)
+HomeScreen(tittle = R.string.homescreen),
+ListScreen(tittle = R.string.app_name),
+FavoriteScreen(tittle = R.string.favorites),
+PlayedScreen(tittle = R.string.played),
+SharedScreen(tittle = R.string.shared),
+RatedScreen(tittle = R.string.rated)
 }*/
 
 /**
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar(
@@ -54,9 +51,9 @@ modifier: Modifier = Modifier,
 TopAppBar(
 title = {
 Text(
-stringResource(id = currentScreenTitle),
+//     stringResource(id = currentScreenTitle),
 //text = currentScreen.title,
-// text = "Games",
+text = "Games",
 fontWeight = FontWeight.Bold,
 overflow = TextOverflow.Ellipsis,
 )
@@ -68,42 +65,35 @@ if (canNavigateBack) {
 IconButton(onClick = navigateUp) {
 Icon(
 imageVector = Icons.Default.ArrowBack,
-contentDescription = stringResource(id = R.string.back_button)
+contentDescription = null
 )
 }
 }
-/**
-IconButton(onClick = { onMenuClick() }) {
+
+IconButton(onClick = { /**onMenuClick()*/ }) {
 Icon(
 imageVector = Icons.Default.Menu,
 contentDescription = "Menu Icon"
 )
-}*/
-},
-
-)
-
-
-colors = TopAppBarDefaults.topAppBarColors(
-containerColor = MaterialTheme.colors.surfaceColorAtElevation(3.dp)
-
-
 }
- */
+},
+)
+}*/
+
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun GameApp(
     modifier: Modifier = Modifier,
 
-   //top navigation:
-   viewModel: GameViewModel = viewModel(),
-   // navController: NavHostController = rememberNavController()
+    //top navigation:
+    viewModel: GameViewModel = viewModel(),
+    // navController: NavHostController = rememberNavController()
 
 ) {
 
-   // val backStackEntry by navController.currentBackStackEntryAsState()
-   // val currentScreen = NavigationScreens.valueOf(
+    // val backStackEntry by navController.currentBackStackEntryAsState()
+    // val currentScreen = NavigationScreens.valueOf(
     //    backStackEntry?.destination?.route ?: NavigationScreens.ListScreen.name
     //)
 
@@ -112,7 +102,7 @@ fun GameApp(
 
     val navController = rememberNavController()
     val scaffoldState = rememberScaffoldState()
-    val  scope = rememberCoroutineScope()
+    val scope = rememberCoroutineScope()
 
 
     //THIS IS BOTTOM BAR TOP LEVEL DESTINATION:
@@ -123,30 +113,22 @@ fun GameApp(
         Pantalla4,
         Pantalla5,
 
-       )
+        )
 
     Scaffold(
         scaffoldState = scaffoldState,
         modifier = modifier.fillMaxSize(),
 
-        bottomBar = {BottomBar(navController = navController, items = items)}
+        bottomBar = { BottomBar(navController = navController, items = items) },
 /**
-        topBar = {
+                topBar = {
             //TOP BAR COMPOSABLE CUSTOM TO NAVIGATE:
             CustomTopBar(
-                //currentScreen = currentScreen,
-                canNavigateBack = navController.previousBackStackEntry != null,
-                navigateUp = { navController.navigateUp() }
+               // currentScreen = currentScreen,
+                // canNavigateBack = navController.previousBackStackEntry != null,
+               // navigateUp = { navController.navigateUp() }
             )
         }*/
-
-        //BOTTOM NAVIGATION:
-
-        //  modifier = modifier
-        //    .fillMaxSize()
-        //     .nestedScroll(scrollBehavior.nestedScrollConnection),
-
-
 
 
     ) {
@@ -165,13 +147,14 @@ fun GameApp(
         }
     }
 }
-/**
+
+
 @Composable
 fun CustomTopBar(
-    //TopApp Bar navigation:
-   // currentScreen: NavigationScreens,
-   // canNavigateBack: Boolean,
-    //navigateUp: () -> Unit,
+//TopApp Bar navigation:
+//currentScreen: NavigationScreens,
+// canNavigateBack: Boolean,
+//navigateUp: () -> Unit,
 
 
     modifier: Modifier = Modifier
@@ -179,23 +162,23 @@ fun CustomTopBar(
 
     TopAppBar(
 
-        title = { Text(stringResource(R.string.app_name)) },
+        title = { Text(stringResource(id = stringResource(R.string.games).toInt())) },
         contentColor = colorScheme.inverseSurface,
         backgroundColor = colorScheme.surface,
         /**navigationIcon = {
-            if (canNavigateBack) {
-                IconButton(onClick = navigateUp) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = stringResource(R.string.back_button)
-                    )
-                }
-            }
+        if (canNavigateBack) {
+        IconButton(onClick = navigateUp) {
+        Icon(
+        imageVector = Icons.Filled.ArrowBack,
+        contentDescription = stringResource(R.string.back_button)
+        )
+        }
+        }
         },*/
         modifier = modifier,
-        )
+    )
 }
-*/
+
 //BOTTOM NAVIGATION:
 
 @Composable
@@ -210,7 +193,7 @@ fun BottomBar(
     items: List<BottomBarScreen>,
     modifier: Modifier = Modifier,
 ) {
-modifier.background(color = colorScheme.background)
+    modifier.background(color = colorScheme.background)
     //back = colorScheme.background,
     androidx.compose.material3.BottomAppBar() {
         BottomNavigation(
@@ -228,14 +211,14 @@ modifier.background(color = colorScheme.background)
                         Icon(
                             imageVector = item.icon,
                             contentDescription = item.title,
-                            tint= Color.Gray,
-                            )
+                            tint = Color.Gray,
+                        )
                     },
 //If show tittle, add this:
                     label = {
-                        Text( text = item.title)
+                        Text(text = item.title)
                         colorScheme.inverseSurface
-                            },
+                    },
                     alwaysShowLabel = false
                 )
             }
@@ -245,32 +228,32 @@ modifier.background(color = colorScheme.background)
 /**
 @Composable
 fun RowScope.AddItem(
-    screen: BottomBarScreen,
-    currentDestination: NavDestination?,
-    navController: NavHostController
+screen: BottomBarScreen,
+currentDestination: NavDestination?,
+navController: NavHostController
 ) {
-    BottomNavigationItem(
-        label = {
-            Text(text = screen.title)
-        },
-        icon = {
-            Icon(
-                imageVector = screen.icon,
-                contentDescription = "Navigation Icon"
-            )
-        },
-        selected = currentDestination?.hierarchy?.any {
-            it.route == screen.route
-        } == true,
-        unselectedContentColor = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
-        onClick = {
-            navController.navigate(screen.route) {
-                popUpTo(navController.graph.findStartDestination().id) {
-                    saveState = true
-                }
-                launchSingleTop = true
-                restoreState = true
-            }
-        }
-    )
+BottomNavigationItem(
+label = {
+Text(text = screen.title)
+},
+icon = {
+Icon(
+imageVector = screen.icon,
+contentDescription = "Navigation Icon"
+)
+},
+selected = currentDestination?.hierarchy?.any {
+it.route == screen.route
+} == true,
+unselectedContentColor = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
+onClick = {
+navController.navigate(screen.route) {
+popUpTo(navController.graph.findStartDestination().id) {
+saveState = true
+}
+launchSingleTop = true
+restoreState = true
+}
+}
+)
 }*/
