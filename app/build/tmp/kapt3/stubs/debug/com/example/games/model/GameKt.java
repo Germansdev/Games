7e24ecaba08a81f2f34b816c5e711791e2fb122b
Germansdev <germanschw@yahoo.com>
2023-06-23 20:45:20 -0300

    db data insert

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/games/model/GameKt.java
@@ -0,0 +1,19 @@
+package com.example.games.model;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 2, d1 = {"\u0000\n\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u001a\n\u0010\u0000\u001a\u00020\u0001*\u00020\u0001\u001a\n\u0010\u0002\u001a\u00020\u0001*\u00020\u0001\u00a8\u0006\u0003"}, d2 = {"asEntity", "Lcom/example/games/model/Game;", "asExternalModel", "app_debug"})
+public final class GameKt {
+    
+    @org.jetbrains.annotations.NotNull
+    public static final com.example.games.model.Game asEntity(@org.jetbrains.annotations.NotNull
+    com.example.games.model.Game $this$asEntity) {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull
+    public static final com.example.games.model.Game asExternalModel(@org.jetbrains.annotations.NotNull
+    com.example.games.model.Game $this$asExternalModel) {
+        return null;
+    }
+}
